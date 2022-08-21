bacd53ac3c1bd270a6da34c4f82f2237433c89ba
kelcho-spense <kelchospense88@gmail.com>
2022-08-21 13:53:12 +0300

    finished the movie project

---
+++ 11.DOM/script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded',function () {
+    const list = document.querySelector("#movie-list ul");
+    const forms = document.forms;
+
+        //delete movies
+    list.addEventListener('click',(e) => {
+        if (e.target.className == 'delete') {
+            const li = e.target.parentElement;
+            li.parentNode.removeChild(li);
+        }
+    })
+        //add movie
+    const addform = forms['add-movie']
+    addform.addEventListener("submit",function(e) {
+        e.preventDefault();        
+    //creating elements
+    const value = addform.querySelector('input[type="text"]').value;
+    const li = document.createElement('li');
+    const movieName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+    //add text content
+    movieName.textContent = value;
+    deleteBtn.textContent = 'delete';
+    //add classes
+    movieName.classList.add('name'); 
+    deleteBtn.classList.add('delete'); 
+    //append to DOM
+    li.appendChild(movieName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+    })
+    
+})
